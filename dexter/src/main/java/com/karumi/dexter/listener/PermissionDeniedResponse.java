88c356d0dda490a8b9f297da217d3b2f374ceded
Alex Florescu <anothem@gmail.com>
2015-11-19 14:32:55 +0000

    Add copyright and cleanup javadoc

--- dexter/src/main/java/com/karumi/dexter/listener/PermissionDeniedResponse.java
+++ dexter/src/main/java/com/karumi/dexter/listener/PermissionDeniedResponse.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Karumi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.karumi.dexter.listener;
 
 import android.support.annotation.NonNull;
@@ -19,9 +35,6 @@ public final class PermissionDeniedResponse {
   /**
    * Builds a new instance of PermissionDeniedResponse from a given permission string
    * and a permanently-denied boolean flag
-   *
-   * @param permission
-   * @return
    */
   public static PermissionDeniedResponse from(@NonNull String permission, boolean permanentlyDenied) {
     return new PermissionDeniedResponse(new PermissionRequest(permission), permanentlyDenied);
