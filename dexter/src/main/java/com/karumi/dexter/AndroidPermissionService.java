426b7cae9550a9d842985adc520251c1ff395e4a
Diego GomezOlvera <diego.gomezolvera@booking.com>
2015-11-26 13:23:37 +0100

    Handle unknown permissions as granted before Marshmallow
    https://commonsware.com/blog/2015/11/09/you-cannot-hold-nonexistent-permissions.html

--- dexter/src/main/java/com/karumi/dexter/AndroidPermissionService.java
+++ dexter/src/main/java/com/karumi/dexter/AndroidPermissionService.java
@@ -16,8 +16,11 @@
 
 package com.karumi.dexter;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -31,7 +34,11 @@ class AndroidPermissionService {
    * @see ContextCompat#checkSelfPermission
    */
   int checkSelfPermission(@NonNull Context context, @NonNull String permission) {
-    return ContextCompat.checkSelfPermission(context, permission);
+    if (isHandledPermission(permission)) {
+      return ContextCompat.checkSelfPermission(context, permission);
+    } else {
+      return PackageManager.PERMISSION_GRANTED;
+    }
   }
 
   /**
@@ -48,5 +55,161 @@ class AndroidPermissionService {
   boolean shouldShowRequestPermissionRationale(@NonNull Activity activity,
       @NonNull String permission) {
     return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission);
+  }
+
+  /**
+   * Before Marshmallow Android only handles properly permissions that are known.
+   * <p/>
+   * https://commonsware.com/blog/2015/11/09/you-cannot-hold-nonexistent-permissions.html
+   *
+   * @param permission Permission to check.
+   * @return True if the platform can handle it, false otherwise.
+   */
+  @SuppressWarnings("deprecation")
+  private static boolean isHandledPermission(@NonNull String permission) {
+    switch (permission) {
+      case Manifest.permission.ACCESS_CHECKIN_PROPERTIES:
+      case Manifest.permission.ACCESS_COARSE_LOCATION:
+      case Manifest.permission.ACCESS_FINE_LOCATION:
+      case Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS:
+      case Manifest.permission.ACCESS_NETWORK_STATE:
+      case Manifest.permission.ACCESS_WIFI_STATE:
+      case Manifest.permission.BATTERY_STATS:
+      case Manifest.permission.BLUETOOTH:
+      case Manifest.permission.BLUETOOTH_ADMIN:
+      case Manifest.permission.BROADCAST_PACKAGE_REMOVED:
+      case Manifest.permission.BROADCAST_STICKY:
+      case Manifest.permission.CALL_PHONE:
+      case Manifest.permission.CALL_PRIVILEGED:
+      case Manifest.permission.CAMERA:
+      case Manifest.permission.CHANGE_COMPONENT_ENABLED_STATE:
+      case Manifest.permission.CHANGE_CONFIGURATION:
+      case Manifest.permission.CHANGE_NETWORK_STATE:
+      case Manifest.permission.CHANGE_WIFI_STATE:
+      case Manifest.permission.CLEAR_APP_CACHE:
+      case Manifest.permission.CONTROL_LOCATION_UPDATES:
+      case Manifest.permission.DELETE_CACHE_FILES:
+      case Manifest.permission.DELETE_PACKAGES:
+      case Manifest.permission.DIAGNOSTIC:
+      case Manifest.permission.DISABLE_KEYGUARD:
+      case Manifest.permission.DUMP:
+      case Manifest.permission.EXPAND_STATUS_BAR:
+      case Manifest.permission.FACTORY_TEST:
+      case Manifest.permission.FLASHLIGHT:
+      case Manifest.permission.GET_ACCOUNTS:
+      case Manifest.permission.GET_PACKAGE_SIZE:
+      case Manifest.permission.GET_TASKS:
+      case Manifest.permission.INSTALL_PACKAGES:
+      case Manifest.permission.INTERNET:
+      case Manifest.permission.MASTER_CLEAR:
+      case Manifest.permission.MODIFY_AUDIO_SETTINGS:
+      case Manifest.permission.MODIFY_PHONE_STATE:
+      case Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS:
+      case Manifest.permission.PERSISTENT_ACTIVITY:
+      case Manifest.permission.PROCESS_OUTGOING_CALLS:
+      case Manifest.permission.READ_CALENDAR:
+      case Manifest.permission.READ_CONTACTS:
+      case Manifest.permission.READ_FRAME_BUFFER:
+      case Manifest.permission.READ_INPUT_STATE:
+      case Manifest.permission.READ_LOGS:
+      case Manifest.permission.READ_PHONE_STATE:
+      case Manifest.permission.READ_SMS:
+      case Manifest.permission.READ_SYNC_SETTINGS:
+      case Manifest.permission.READ_SYNC_STATS:
+      case Manifest.permission.REBOOT:
+      case Manifest.permission.RECEIVE_BOOT_COMPLETED:
+      case Manifest.permission.RECEIVE_MMS:
+      case Manifest.permission.RECEIVE_SMS:
+      case Manifest.permission.RECEIVE_WAP_PUSH:
+      case Manifest.permission.RECORD_AUDIO:
+      case Manifest.permission.REORDER_TASKS:
+      case Manifest.permission.RESTART_PACKAGES:
+      case Manifest.permission.SEND_SMS:
+      case Manifest.permission.SET_ALWAYS_FINISH:
+      case Manifest.permission.SET_ANIMATION_SCALE:
+      case Manifest.permission.SET_DEBUG_APP:
+      case Manifest.permission.SET_PREFERRED_APPLICATIONS:
+      case Manifest.permission.SET_PROCESS_LIMIT:
+      case Manifest.permission.SET_TIME_ZONE:
+      case Manifest.permission.SET_WALLPAPER:
+      case Manifest.permission.SET_WALLPAPER_HINTS:
+      case Manifest.permission.SIGNAL_PERSISTENT_PROCESSES:
+      case Manifest.permission.STATUS_BAR:
+      case Manifest.permission.SYSTEM_ALERT_WINDOW:
+      case Manifest.permission.VIBRATE:
+      case Manifest.permission.WAKE_LOCK:
+      case Manifest.permission.WRITE_APN_SETTINGS:
+      case Manifest.permission.WRITE_CALENDAR:
+      case Manifest.permission.WRITE_CONTACTS:
+      case Manifest.permission.WRITE_GSERVICES:
+      case Manifest.permission.WRITE_SETTINGS:
+      case Manifest.permission.WRITE_SYNC_SETTINGS:
+        return true; //API 1
+      case Manifest.permission.BROADCAST_SMS:
+      case Manifest.permission.BROADCAST_WAP_PUSH:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.BASE_1_1;
+      case Manifest.permission.BIND_APPWIDGET:
+      case Manifest.permission.BIND_INPUT_METHOD:
+      case Manifest.permission.MOUNT_FORMAT_FILESYSTEMS:
+      case Manifest.permission.UPDATE_DEVICE_STATS:
+      case Manifest.permission.WRITE_SECURE_SETTINGS:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE;
+      case Manifest.permission.CHANGE_WIFI_MULTICAST_STATE:
+      case Manifest.permission.GLOBAL_SEARCH:
+      case Manifest.permission.INSTALL_LOCATION_PROVIDER:
+      case Manifest.permission.WRITE_EXTERNAL_STORAGE:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT;
+      case Manifest.permission.ACCOUNT_MANAGER:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR;
+      case Manifest.permission.BIND_DEVICE_ADMIN:
+      case Manifest.permission.BIND_WALLPAPER:
+      case Manifest.permission.KILL_BACKGROUND_PROCESSES:
+      case Manifest.permission.SET_TIME:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+      case Manifest.permission.NFC:
+      case Manifest.permission.SET_ALARM:
+      case Manifest.permission.USE_SIP:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+      case Manifest.permission.BIND_REMOTEVIEWS:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+      case Manifest.permission.ADD_VOICEMAIL:
+      case Manifest.permission.BIND_TEXT_SERVICE:
+      case Manifest.permission.BIND_VPN_SERVICE:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+      case Manifest.permission.BIND_ACCESSIBILITY_SERVICE:
+      case Manifest.permission.READ_EXTERNAL_STORAGE:
+      case Manifest.permission.READ_CALL_LOG:
+      case Manifest.permission.WRITE_CALL_LOG:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+      case Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE:
+      case Manifest.permission.LOCATION_HARDWARE:
+      case Manifest.permission.SEND_RESPOND_VIA_MESSAGE:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+      case Manifest.permission.BIND_NFC_SERVICE:
+      case Manifest.permission.BIND_PRINT_SERVICE:
+      case Manifest.permission.BLUETOOTH_PRIVILEGED:
+      case Manifest.permission.CAPTURE_AUDIO_OUTPUT:
+      case Manifest.permission.CAPTURE_SECURE_VIDEO_OUTPUT:
+      case Manifest.permission.CAPTURE_VIDEO_OUTPUT:
+      case Manifest.permission.INSTALL_SHORTCUT:
+      case Manifest.permission.MANAGE_DOCUMENTS:
+      case Manifest.permission.MEDIA_CONTENT_CONTROL:
+      case Manifest.permission.TRANSMIT_IR:
+      case Manifest.permission.UNINSTALL_SHORTCUT:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+      case Manifest.permission.BODY_SENSORS:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH;
+      case Manifest.permission.BIND_DREAM_SERVICE:
+      case Manifest.permission.BIND_TV_INPUT:
+      case Manifest.permission.BIND_VOICE_INTERACTION:
+      case Manifest.permission.READ_VOICEMAIL:
+      case Manifest.permission.WRITE_VOICEMAIL:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+      case Manifest.permission.BIND_CARRIER_MESSAGING_SERVICE:
+      case Manifest.permission.BIND_CARRIER_SERVICES:
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1;
+      default:
+        return true; //API 23+
+    }
   }
 }
