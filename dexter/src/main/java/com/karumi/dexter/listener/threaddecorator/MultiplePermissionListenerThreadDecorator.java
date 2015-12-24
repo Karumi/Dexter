d4e0fb81370fe1281c879bff14a66e7356d4d978
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2015-12-24 14:28:17 +0100

    add javadoc

--- dexter/src/main/java/com/karumi/dexter/listener/threaddecorator/MultiplePermissionListenerThreadDecorator.java
+++ dexter/src/main/java/com/karumi/dexter/listener/threaddecorator/MultiplePermissionListenerThreadDecorator.java
@@ -6,6 +6,9 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import java.util.List;
 
+/**
+ * Decorator to execute the permission updates on a given thread specification
+ */
 public class MultiplePermissionListenerThreadDecorator implements MultiplePermissionsListener {
 
   private final MultiplePermissionsListener listener;
@@ -17,19 +20,30 @@ public class MultiplePermissionListenerThreadDecorator implements MultiplePermis
     this.listener = listener;
   }
 
-  @Override
-  public void onPermissionsChecked(final MultiplePermissionsReport multiplePermissionsReport) {
+  /**
+   * Decorates de permission listener execution with a given thread spec
+   *
+   * @param report In detail report with all the permissions that has been denied and granted
+   */
+  @Override public void onPermissionsChecked(final MultiplePermissionsReport report) {
     threadSpec.execute(new Runnable() {
       @Override public void run() {
-        listener.onPermissionsChecked(multiplePermissionsReport);
+        listener.onPermissionsChecked(report);
       }
     });
   }
 
-  @Override
-  public void onPermissionRationaleShouldBeShown(final List<PermissionRequest> permissions,
-      final PermissionToken token) {
+  /**
+   * The method is intended to do some UI modification, therefore the method is not decorated with a
+   * thread spec and will execute on the main thread.
+   *
+   * @param permissions The permissions that has been requested. Collections of values found in
+   * {@link android.Manifest.permission}
+   * @param token Token used to continue or cancel the permission request process. The permission
+   * request process will remain blocked until one of the token methods is called
+   */
+  @Override public void onPermissionRationaleShouldBeShown(
+      final List<PermissionRequest> permissions, final PermissionToken token) {
     listener.onPermissionRationaleShouldBeShown(permissions, token);
   }
-
 }
