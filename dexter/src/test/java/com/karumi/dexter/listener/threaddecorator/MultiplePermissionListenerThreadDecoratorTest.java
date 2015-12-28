a82895d104a523aa83e56f315107c1791d03ec13
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2015-12-28 17:59:23 +0100

    checkstyle

--- dexter/src/test/java/com/karumi/dexter/listener/threaddecorator/MultiplePermissionListenerThreadDecoratorTest.java
+++ dexter/src/test/java/com/karumi/dexter/listener/threaddecorator/MultiplePermissionListenerThreadDecoratorTest.java
@@ -28,7 +28,6 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static java.util.Collections.singletonList;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
