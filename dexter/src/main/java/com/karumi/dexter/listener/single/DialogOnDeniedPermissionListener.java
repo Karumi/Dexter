10b74d0ad5d14b4c3884c8e0bdd5414ba7c55f69
Sergio Gutierrez <sergio@gokarumi.com>
2016-12-23 14:40:15 +0100

    Revert "Show a rationale when using a default DialogOnDeniedPermission (single and multi) (#59)"
    This reverts commit b75aaa9d515316a44186e8da3b16073792a568a4.

--- dexter/src/main/java/com/karumi/dexter/listener/single/DialogOnDeniedPermissionListener.java
+++ dexter/src/main/java/com/karumi/dexter/listener/single/DialogOnDeniedPermissionListener.java
@@ -22,10 +22,7 @@ import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.StringRes;
-
-import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
 
 /**
  * Utility listener that shows a {@link android.app.Dialog} with a minimum configuration when the
@@ -61,11 +58,6 @@ public class DialogOnDeniedPermissionListener extends EmptyPermissionListener {
         })
         .setIcon(icon)
         .show();
-  }
-
-  @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission,
-                                                           PermissionToken token) {
-    token.continuePermissionRequest();
   }
 
   /**
